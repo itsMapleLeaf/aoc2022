94ebb22da8eacd5127bed116b9952f195229e5c8
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-12-02 10:59:14 -0600

    sugar is lame

--- src/day1.nim
+++ src/day1.nim
@@ -2,20 +2,19 @@ import strutils
 import sequtils
 import algorithm
 import helpers
-import sugar
 
 const input = "src/day1.txt"
 
 proc part1(): int =
   input.readFile().strip()
     .split("\n\n")
-    .map(nums => nums.split("\n").map(parseInt).sum())
+    .mapIt(it.split("\n").map(parseInt).sum())
     .max()
 
 proc part2(): int =
   input.readFile().strip()
     .split("\n\n")
-    .map(nums => nums.split("\n").map(parseInt).sum())
+    .mapIt(it.split("\n").map(parseInt).sum())
     .sorted(Descending)[0 .. 2]
     .sum()
     
