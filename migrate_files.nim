d6024bcdf4315c0f129e4dc3437a1518ee5c80a6
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-12-11 17:43:16 -0600

    remove file migrate script

--- migrate_files.nim
+++
@@ -1,7 +0,0 @@
-import os
-
-for file in walkFiles("src/*.nim"):
-  let (dir, name, ext) = file.splitFile
-  createDir name
-  moveFile file, name / "solution.nim"
-  moveFile (dir / name & ".txt"), name / "input.txt"
